161ba884b493c635e914cfc77795f52803fbac5f
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:49:25 +0100

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
   has_many :likes
-  has_many :dislikes
+  has_many :dislikes # Mmmmh, c'est curieux. Je vais essayer de vous suivre mais votre conception est... étrange
 
-  belongs_to :user
+  belongs_to :user # Bien
 
-  validates :content, presence: true
+  validates :content, presence: true # BIEN ! Merci d'être le premier TP où je vois (enfin) cette validation
 end
